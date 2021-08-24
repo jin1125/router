241696ed015a5f40c0a55ba21a33560a9deea154
jin1125 <shogo.jinta@gmail.com>
2021-08-25 00:55:16 +0900

    pracrice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <h1>react-routerに入門する。</h1>
+    <nav>
+      <ul>
+        <li><Link to='/'>Index</Link></li>
+        <li><Link to='/second'>Second</Link></li>
+        <li><Link to='/third'>Third</Link></li>
+      </ul>
+    </nav>
+    <article>
+      <Route exact path='/' component={Index} />
+      <Route path='/second' component={Second} />
+      <Route path='/third' component={Third} />
+    </article>
+  </BrowserRouter>
+)
 
-export default App;
+const Index = () => (
+  <div>
+    <h2>Index</h2>
+    <p>インデックスページ</p>
+  </div>
+)
+const Second = () => (
+  <div>
+    <h2>Second</h2>
+    <p>二番目のページです</p>
+  </div>
+)
+const Third = () => (
+  <div>
+    <h2>Third</h2>
+    <p>三番目のページです</p>
+  </div>
+)
+
+export default App
